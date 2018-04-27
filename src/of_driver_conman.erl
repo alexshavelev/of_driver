0578f1991b3f21457769f08ca22de53ff0582fb1
Alexander Shavelev <alexshavelev44@gmail.com>
2018-04-27 15:48:02 +0300

    Update of_driver_conman.erl

--- src/of_driver_conman.erl
+++ src/of_driver_conman.erl
@@ -76,7 +76,7 @@ handle_cast(connect, State = #?STATE{ipaddr = IpAddr, port = Port}) ->
         {ok, Socket} ->
             case of_driver_connection_sup:start_child(Socket) of
                 {ok, Connection} ->
-                    {ok, DriverQueuePid} = of_driver_queue_sup:start_child(Connection),
+%                     {ok, DriverQueuePid} = of_driver_queue_sup:start_child(Connection),
                     gen_tcp:controlling_process(Socket, Connection),
                     %%gen_tcp:controlling_process(Socket, DriverQueuePid),
                     MonitorRef = erlang:monitor(process, Connection),
